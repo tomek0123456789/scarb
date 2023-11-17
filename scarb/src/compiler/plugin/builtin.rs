b61cf56fed2cbc35c7b5d251c368bb97ae4de035
maciektr <maciej.tratnowiecki@swmansion.com>
2023-11-17 18:16:05 +0100

    Libload test plugin

--- scarb/src/compiler/plugin/builtin.rs
+++ scarb/src/compiler/plugin/builtin.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use cairo_lang_defs::plugin::PluginSuite;
 use cairo_lang_starknet::starknet_plugin_suite;
-use cairo_lang_test_plugin::test_plugin_suite;
+use std::ffi::OsString;
+// use cairo_lang_test_plugin::test_plugin_suite;
 
 use crate::compiler::plugin::{CairoPlugin, CairoPluginInstance};
 use crate::core::{PackageId, PackageName, SourceId};
@@ -41,14 +42,35 @@ impl CairoPlugin for BuiltinTestPlugin {
     }
 
     fn instantiate(&self) -> Result<Box<dyn CairoPluginInstance>> {
-        Ok(Box::new(BuiltinTestPluginInstance))
+        Ok(Box::new(BuiltinTestPluginInstance::new()))
     }
 }
 
-struct BuiltinTestPluginInstance;
+struct BuiltinTestPluginInstance {
+    lib: libloading::Library,
+}
+
+impl BuiltinTestPluginInstance {
+    pub fn new() -> Self {
+        let library_path: OsString = std::env::var_os("SCARB_DYLIB").unwrap_or_else(|| {
+            OsString::from(
+                "/Users/maciektr/Projects/scarb-plugins/dylib/target/debug/libplugin.dylib",
+            )
+        });
+        let lib = unsafe { libloading::Library::new(library_path).unwrap() };
+        Self { lib }
+    }
+}
 
 impl CairoPluginInstance for BuiltinTestPluginInstance {
     fn plugin_suite(&self) -> PluginSuite {
-        test_plugin_suite()
+        let f = unsafe {
+            self.lib
+                .get::<fn() -> PluginSuite>(b"test_plugin_suite\0")
+                .unwrap()
+        };
+        let p = f();
+        dbg!(&p);
+        p
     }
 }
